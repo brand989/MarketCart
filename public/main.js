7d4c968a9411c5bcab5285aca75ec712c8869015
brand989 <brand989@gmail.com>
2020-11-23 11:26:27 +0300

    finish firdtlesson

--- public/main.js
+++ public/main.js
@@ -1,273 +1,331 @@
-var abc = "abcdefghijklmnopqrstuvwxyz";
-       
-class Product {
+       class Product {
 
-    name = ''
-    price = ''
-    count = '1'
+           name = ''
+           price = ''
+           count = '1'
 
 
-    constructor(name, price) {
-        this.name = name
-        this.price = price
-    }
+           constructor(name, price) {
+               this.name = name
+               this.price = price
+           }
 
 
-    renderMain() {
-        const {
-            name,
-            price
-        } = this
+           renderMain() {
+               const {
+                   name,
+                   price
+               } = this
 
 
-        let item = document.createElement('li')
-        item.classList.add('product-list-element')
-        item.innerHTML = `
+               let item = document.createElement('li')
+               item.classList.add('product-list-element')
+               item.innerHTML = `
     <span>${name} ${price} руб. </span>
     `
-        item.appendChild(this.getAddInCartBtn())
+               item.appendChild(this.getAddInCartBtn())
 
-        return item
+               return item
 
-    }
+           }
 
 
 
-    renderCart() {
-        const {
-            name,
-            price,
-            count
-        } = this
+           renderCart() {
+               const {
+                   name,
+                   price,
+                   count
+               } = this
 
-        let item = document.createElement('div')
-        item.classList.add('cart-list')
-        item.innerHTML = `
+               let item = document.createElement('div')
+               item.classList.add('cart-list')
+               item.innerHTML = `
         <span>${name} ${price} руб.${count}шт. </span>
         `
-        item.appendChild(this.getMinusCartBtn())
-        item.appendChild(this.getAddInCartBtn())
-        item.appendChild(this.getDeleteCartBtn())
+               item.appendChild(this.getMinusCartBtn())
+               item.appendChild(this.getAddInCartBtn())
+               item.appendChild(this.getDeleteCartBtn())
+
 
+               return item
 
-        return item
+           }
 
-    }
 
+           getAddInCartBtn() {
+               const btn = document.createElement('button')
+               btn.classList.add('btn')
 
-    getAddInCartBtn() {
-        const btn = document.createElement('button')
-        btn.classList.add('btn')
+               btn.innerHTML = '+'
 
-        btn.innerHTML = '+'
+               let promise = new Promise((resolve, reject) => {
 
-        btn.addEventListener('click', () => {
-            const userCart = new Cart()
-            userCart.add(this)
-        })
+                   btn.addEventListener('click', () => {
+                       const userCart = new Cart()
+                       userCart.add(this)
+                       resolve("товар добавлен в корзину");
+                   })
+               });
 
-        return btn
-    }
+               promise
+                   .then(
+                       result => {
+                           console.log(result);
+                       },
+                       error => {
+                           alert("упс" + error);
+                       }
+                   );
 
-    inc() {
-        this.count++
-    }
+               return btn
+           }
 
-    dec() {
-        this.count--
-    }
+           inc() {
+               this.count++
+           }
 
-    getMinusCartBtn() {
-        const btn = document.createElement('button')
-        btn.classList.add('btn')
-        btn.innerHTML = '-'
+           dec() {
+               this.count--
+           }
 
-        btn.addEventListener('click', () => {
-            const userCart = new Cart()
-            userCart.remove(this)
-        })
+           getMinusCartBtn() {
+               const btn = document.createElement('button')
+               btn.classList.add('btn')
+               btn.innerHTML = '-'
 
+               let promise = new Promise((resolve, reject) => {
 
-        return btn
+                   btn.addEventListener('click', () => {
+                       const userCart = new Cart()
+                       userCart.remove(this)
+                       resolve(" уменьшили количество товаров в корзине");
+                   })
+               });
 
-    }
+               promise
+                   .then(
+                       result => {
+                           console.log(result);
+                       },
+                       error => {
+                           alert("упс" + error);
+                       }
+                   );
 
-    getDeleteCartBtn() {
-        const btn = document.createElement('button')
-        btn.classList.add('btn')
-        btn.innerHTML = 'Удалить из корзины'
 
-        btn.addEventListener('click', () => {
-            const userCart = new Cart()
-            userCart.delete(this)
-        })
 
-        return btn
-    }
-}
+               return btn
 
+           }
 
+           getDeleteCartBtn() {
+               const btn = document.createElement('button')
+               btn.classList.add('btn')
+               btn.innerHTML = 'Удалить из корзины'
 
+               let promise = new Promise((resolve, reject) => {
 
-class List {
+                   btn.addEventListener('click', () => {
+                       const userCart = new Cart()
+                       userCart.delete(this)
+                       resolve(" удален товар из корзины");
+                   })
+               });
 
-    items = []
+               promise
+                   .then(
+                       result => {
+                           console.log(result);
+                       },
+                       error => {
+                           alert("упс" + error);
+                       }
+                   );
 
-  
+               return btn
+           }
+       }
 
 
-    add(item) {
 
-        if (this.findProduct(item)) {
-            item.inc()
-        } else {
-            this.items.push(item)
-        }
 
+       class List {
 
-        this.render()
-    }
+           items = []
 
 
-    findProduct(product) {
-        return this.items.filter(item => item.name === product.name)[0]
-    }
 
 
-    remove(item) {
+           add(item) {
 
-        if (!this.findProduct(item)) {
-            return
-        }
+               if (this.findProduct(item)) {
+                   item.inc()
+               } else {
+                   this.items.push(item)
+               }
 
 
-        if (item.count > 1) {
-            item.dec()
-        } else {
-            this.items = this.items.filter(good => item.name !== good.name)
-        }
+               this.render()
+           }
 
-        this.render()
 
-    }
+           findProduct(product) {
+               return this.items.filter(item => item.name === product.name)[0]
+           }
 
-    delete(item) {
 
-        if (!this.findProduct(item)) {
-            return
-        }
+           remove(item) {
 
+               if (!this.findProduct(item)) {
+                   return
+               }
 
-        this.items = this.items.filter(good => item.name !== good.name)
-        this.render()
-    }
 
-    render() {
+               if (item.count > 1) {
+                   item.dec()
+               } else {
+                   this.items = this.items.filter(good => item.name !== good.name)
+               }
 
-    }
+               this.render()
 
-}
+           }
 
+           delete(item) {
 
-class Cart extends List {
+               if (!this.findProduct(item)) {
+                   return
+               }
 
-    constructor(items) {
-        if (Cart._instance) {
-            return Cart._instance
-        }
-        super(items)
-        Cart._instance = this
-    }
 
+               this.items = this.items.filter(good => item.name !== good.name)
+               this.render()
+           }
 
+           render() {
 
-    render() {
-        const placeToRender = document.querySelector('.cart')
-        placeToRender.innerHTML = '<h1>Коризина</h1>' + this.calcSumCart()
-        this.items.forEach(item => placeToRender.appendChild(item.renderCart()));
+           }
 
-    }
+       }
 
 
-    calcSumCart() {
-        let sum = this.items.reduce((sum, cur) => sum + cur.price * cur.count, 0);
-        const placeToRender = document.querySelector('.cart')
+       class Cart extends List {
 
-        return `<p>Товаров на сумму ${sum}<p>`
+           constructor(items) {
+               if (Cart._instance) {
+                   return Cart._instance
+               }
+               super(items)
+               Cart._instance = this
+           }
 
 
 
-    }
+           render() {
+               const placeToRender = document.querySelector('.cart')
+               placeToRender.innerHTML = '<h1>Коризина</h1>' + this.calcSumCart()
+               this.items.forEach(item => placeToRender.appendChild(item.renderCart()));
 
-}
+           }
 
 
-class ProductList extends List {
+           calcSumCart() {
+               let sum = this.items.reduce((sum, cur) => sum + cur.price * cur.count, 0);
+               const placeToRender = document.querySelector('.cart')
 
+               return `<p>Товаров на сумму ${sum}<p>`
 
-    constructor (items) {
-        super(items)
-        this.addGoodsList(10)
-      }
+           }
 
+       }
 
-    addGoodsList(countProduct){
-       
-        let goods = this.fetchGoods(countProduct)
-        goods = goods.map(cur => {
-          return new Product(cur.name, cur.price, 1)
-        })
-        this.items.push(...goods)
-        this.render()
-    }
 
+       class ProductList extends List {
 
-    fetchGoods (count) {
-       
-        let arr = []
-        for(let i = 0; i < count; i++) {
-        let nameItem = "";
-        while (nameItem.length < Math.floor(Math.random()*(6-3)+3)) {
-            nameItem += abc[Math.floor(Math.random() * abc.length)];
-        } 
-        let priceItem = Math.floor(Math.random()*(100 - 1) + 1)
-        
-        arr.push({name:nameItem, price:priceItem})
-        }
 
+           constructor(items) {
+               super(items)
+               let goodsPromise = this.fetchGoods('http://localhost:4000/database.json')
+               goodsPromise.then(() => {
+                   this.render()
+               })
+           }
 
-        
-        return arr
 
-    }
 
+           fetchGoods(url) {
 
+               const result = fetch(url)
+               return result
 
-    render() {
-        const placeToRender = document.querySelector('.product-list')
-        placeToRender.innerHTML = ''
-        this.items.forEach(item => placeToRender.appendChild(item.renderMain()));
-        placeToRender.appendChild(this.getAddProduct());
+                   .then(result => {
+                       return result.json()
+                   })
 
-    }
 
-    getAddProduct() {
-        const btn = document.createElement('button')
-        btn.classList.add('btn')
-        btn.innerHTML = 'Еще товары'
+                   .then(data => {
+                       console.log(data)
+                       let goods = data.data.map(cur => {
+                           return new Product(cur.name, cur.price, 1)
+                       })
+                       this.items.push(...goods)
+                   })
 
-        btn.addEventListener('click', () => {
-            this.addGoodsList(2)
-        })
+                   .catch(e => {
+                       console.log(e)
+                   })
 
+           }
 
-        return btn
 
-    }
 
+           render() {
+               const placeToRender = document.querySelector('.product-list')
+               placeToRender.innerHTML = ''
+               this.items.forEach(item => placeToRender.appendChild(item.renderMain()));
+               placeToRender.appendChild(this.getAddProduct());
 
+           }
 
-}
 
 
-const NewPproductList = new ProductList();
+           getAddProduct() {
+               const btn = document.createElement('button')
+               btn.classList.add('btn')
+               btn.innerHTML = 'Еще товары'
+
+               var i = 1;
+
+               let promise = new Promise((resolve, reject) => {
+
+                    btn.addEventListener('click', () => {
+                        resolve(i++);
+
+                    })
+                });
+
+                promise.then(result => {
+                            let goodsPromise = this.fetchGoods(`http://localhost:4000/database1.json`)
+                            goodsPromise.then(() => {
+                                this.render()
+                            })
+                            console.log(result);
+                        },
+                        error => {
+                            alert("упс" + error);
+                        }
+                    );
+
+
+
+            return btn
+
+           }
+
+
+
+       }
+
+
+       const NewPproductList = new ProductList();
